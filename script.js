1e1ff95b2729a908b6f98c7dc58991d23e292684
adg58 <adg58@njit.edu>
2024-11-20 15:39:59 -0500

    questions

---
+++ script.js
@@ -0,0 +1,27 @@
+var age=20;
+
+var age;
+
+age=100;
+let age=20;
+console.log(age);
+let userName="Harry";
+userName="Potter";
+
+let FirstName= "Ana"
+
+let pets=true;
+
+console.log(pets);
+let house=null;
+console.log(house)
+let LastName="Gonzalez"
+
+console.log(FirstName + ""+ LastName);
+console.log(age);
+console.log(userName);
+
+const valuePi=3.14
+
+console.log(valuePi);
+
